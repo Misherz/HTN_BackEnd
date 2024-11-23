8ecd4510cc8053ba60026d4fa5f5c59169e36d47
Misherz <michelle.j.lugo@gmail.com>
2024-11-23 14:45:06 -0500

    new entry post

--- route/entryRoutes.mjs
+++ route/entryRoutes.mjs
@@ -2,3 +2,19 @@ import express from 'express';
 import journalEntries from '../models/entriesSchema.mjs';
 
 const router = express.Router();
+
+// Create
+router.post('/journal', async (req, res) => {
+    try {
+
+        let newEntry = new Entries(req.body);
+
+        await newEntry.save();
+
+        res.status(200).json(newEntry);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({msg: 'Server error'});
+    }
+});
