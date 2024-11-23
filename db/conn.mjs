2839a2ceaf049c743f52723612ac64ecee5705ee
Misherz <michelle.j.lugo@gmail.com>
2024-11-23 10:35:29 -0500

    export connectDB

--- db/conn.mjs
+++ db/conn.mjs
@@ -3,3 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const connectionString = process.env.mongoURI;
+
+export default async function connectDB() {
+    try {
+        await mongoose.connect(connectionString);
+        console.log(`MongoDB Connected`)
+    } catch (err) {
+        console.error(err);
+        process.exit(1) //if you cant fully connect properly, shutdown the full db
+        
+    }
+}
