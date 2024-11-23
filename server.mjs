445e639eb0501d0252959b063756ef75fb5d87d1
Misherz <michelle.j.lugo@gmail.com>
2024-11-23 10:28:34 -0500

    comments

--- server.mjs
+++ server.mjs
@@ -1,3 +1,23 @@
 import express from "express";
 import bodyParser from 'body-parser';
 import dotenv from "dotenv"
+
+//import DB connections
+
+//import routes
+
+//import data
+
+//schemas
+
+//setup
+
+//connect()
+
+//middleware
+
+//routes
+
+//seed 
+
+//listener
